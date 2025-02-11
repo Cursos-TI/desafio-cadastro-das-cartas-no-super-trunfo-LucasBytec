6da35ecdedda90f26fe76b1676961eb4ab83cb61
LucasBytec <lucasmaracanau2005@gmail.com>
2025-02-11 23:59:26 +0000

    atividade feita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
+    char cidade[20] = " ";
+    int populacao = 0;
+    float area = 0;
+    float pib = 0;
+    int P_turisticos = 0;
+    //Declarando variáveis para cada campo que será preenchido pelo usuário
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Bem-vindo(a) ao Super Trunfo de Cidades, comece preenchendo os respectivos dados:\n");
+
+    printf("Cidade escolhida: %s\n", cidade);
+    scanf(" %s", &cidade);
+    printf("Nome confirmado: %s\n", cidade);
+    //solicitação do preenchimento do campo Cidade e retorno do valor confirmado
+
+    printf("\nPopulação da Cidade: %i\n", populacao);
+    scanf(" %i", &populacao);
+    printf("valor confirmado: %i\n", populacao);
+    //solicitação do preenchimento do campo População e retorno do valor confirmado
+
+    printf("\nÁrea da Cidade (Km²): %.1f\n", area);
+    scanf(" %f", &area);
+    printf("valor confirmado: %.2f\n", area);    
+    //solicitação do preenchimento do campo Área e retorno do valor confirmado
+
+    printf("\nPIB da Cidade: %.1f\n", pib);
+    scanf(" %f", &pib);
+    printf("valor confirmado: %.2f\n", pib);    
+    //solicitação do preenchimento do campo PIB e retorno do valor confirmado
+
+    printf("\nQuantidade de Pontos turísticos da Cidade: %i\n", P_turisticos);
+    scanf(" %i", &P_turisticos);
+    printf("valor confirmado: %i\n", P_turisticos);    
+    //solicitação do preenchimento do campo Pontos turísticos e retorno do valor confirmado
+
+    printf("\nOs dados da sua carta são os seguintes:\n");
+    printf("\n Cidade: %s.\n População: %i.\n Área: %.2f.\n PIB: %.2f.\n Pontos turísticos: %i.\n", cidade, populacao, area, pib, P_turisticos);
 
     return 0;
 }
